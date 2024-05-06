f11c89b447dcb72d601d774ac0328be9b5f5736c
Samantha Samarua <samantha.samarua@gmail.com>
2024-05-06 15:16:03 +0930

    database.js edits - Added Put DB logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.error('Put to the database');
+
+// Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+ 
+// Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+  
+// Open up the desired object store.
+  const store = tx.objectStore('todos');
+
+// Use the .putAll() method to get all data in the database.
+  const request = store.put({ id: id, content: content });
+
+// Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result);
+  
+  return result;
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  console.error('Get all from database');
 
    // Create a connection to the database database and version we want to use.
    const jateDb = await openDB('jate', 1);
